321301004bf75d493dba22d7f19b6ff1c77e77f9
Divyanshi Rastogi <43536638+Divya-5@users.noreply.github.com>
2021-09-25 12:00:54 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useState } from "react";
+import "./styles.css";
+const emojiDictionary = {
+  "🙈": "See-No-Evil Monkey",
+  "🙉": "Hear-No-Evil Monkey",
+  "🙊": "Speak-No-Evil Monkey",
+  "💥": "Collision",
+  "💫": "Dizzy",
+  "💦": "Sweat Droplets",
+  "💨": "Dashing Away",
+  "🐵": "Monkey Face"
+};
+var emojiAvailable = Object.keys(emojiDictionary);
+export default function App() {
+  const [meaning, emojiMeaning] = useState("");
+  function emojiInputSection(event) {
+    var userInput = event.target.value;
+    var meaning = emojiDictionary[userInput];
+    emojiMeaning(meaning);
+    if (meaning === undefined) {
+      meaning = "it doesnot exist in database";
+    }
+    emojiMeaning(meaning);
+  }
+  function emojiClickHandler(emoji) {
+    var meaning = emojiDictionary[emoji];
+    emojiMeaning(meaning);
+  }
+
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <input onChange={emojiInputSection} />
+      <div>{meaning}</div>
+      <h3>Emoji List</h3>
+      {emojiAvailable.map(function (emoji) {
+        return (
+          <span
+            onClick={() => emojiClickHandler(emoji)}
+            style={{ fontSize: "2rem", padding: "0.5rem", cursor: "pointer" }}
+            key={emoji}
+          >
+            {emoji}
+          </span>
+        );
+      })}
+    </div>
+  );
+}
